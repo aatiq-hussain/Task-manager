905316421da795f811c21818f42605f437e3ac66
aatiq-hussain <mr.aatiqhussain@gmail.com>
2023-02-22 13:47:09 +0530

    new

---
+++ client/src/components/Form/styles.js
@@ -0,0 +1,31 @@
+
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(2),
+    background:'#FFF0F8'
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  fileInput: {
+    width: '97%',
+    margin: '10px 0',
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+    background:'#2C026A'
+  },
+  buttonUpload: {
+    
+    background:'#F6007F'
+  },
+}));
